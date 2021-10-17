7655f3853aeb9d7bed9c22672949039b6c4581c6
Ausaf-Pro <ausafhussain010@gmail.com>
2021-10-17 11:01:06 +0530

    frontend application created

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./index";
-// import Login from "./Components/Assignments/Assignment7/Login";
-import ListUser from "./Components/Assignments/Assignment8/ListUser";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ListUser />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
